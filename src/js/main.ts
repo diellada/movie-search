687f5c3a54bb197c5ee37cbe2dad9fa0eb1dab3a
Diella Agyeman <diellaagyeman@hotmail.co.uk>
2019-10-22 23:14:54 +0100

    styling updates

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,29 +1,41 @@
 import { getMovies } from './omdb/omdb';
+
+const updateMovies = (cardConts) => {
+  let rangeObj = new Range();
+  rangeObj.selectNodeContents(cardConts);
+  rangeObj.deleteContents();
+  console.log(cardConts);
+  return cardConts;
+}
   
-const buildList = (movies: string[]) => {
+const buildList = (movies) => {
   const cardContainer = document.getElementById("movie-container");
-  // updateMovies(cardContainer);
-  $("#movie-container").empty();
-  movies.forEach((movie:any) => {
+  updateMovies(cardContainer);
+  // $("#movie-container").empty();
+  movies.forEach((movie) => {
     let card = document.createElement("div");
     card.classList.add("card");
     let paragraph = document.createElement("p");
+    // let cardText = document.createElement("div");
+    // cardText.classList.add("card-text");
     card.append(paragraph);
+    // card.append(cardText);
     cardContainer.append(card);
     card.style.backgroundImage = `url(${movie.Poster})`;
-    paragraph.innerHTML = `${movie.Title}, ${movie.Year}`;
+    paragraph.innerHTML = `${movie.Title} (${movie.Year})`;
+    // cardText.innerHTML = `${movie.Plot}`;
+    // console.log(cardContainer);
   });
 }
 
-// const updateMovies = (cardCont) => {
-//   return cardCont.removeChild();
-// }
+
 
 const searchMovies = () => {
   const searchBar = document.getElementById("search-bar") as HTMLInputElement;
   getMovies(searchBar.value).then((data) => {
     let movieArray:string[] = data.Search;
     buildList(movieArray);
+    console.log(movieArray);
   });
 }
 
